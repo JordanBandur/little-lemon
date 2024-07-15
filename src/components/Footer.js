589e00b9a476f2c81fa96d804b67d3687ef81c28
JordanBandur <jordanbandur@hotmail.ca>
2024-07-15 10:25:16 -0600

    Add Html to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 function Footer() {
   return (
     <footer>
-
+      <section>
+        <img src="images/logo.svg" alt="Company Logo" />
+      </section>
+      <section>
+        <nav aria-label="Doormat Navigation">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#order-online">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+          </ul>
+        </nav>
+      </section>
+      <section>
+        <h2>Contact</h2>
+        <address>
+          <p>Address</p>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </address>
+      </section>
+      <section>
+        <h2>Social Media Links</h2>
+        <ul>
+          <li><a href="#facebook">Facebook</a></li>
+          <li><a href="#twitter">Twitter</a></li>
+          <li><a href="#instagram">Instagram</a></li>
+        </ul>
+      </section>
     </footer>
   );
 }
