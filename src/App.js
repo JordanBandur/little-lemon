b690cfc13063d809446b35ed206f40850718c32b
JordanBandur <jordanbandur@hotmail.ca>
2024-07-15 08:53:38 -0600

    Update App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Homepage</p>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
