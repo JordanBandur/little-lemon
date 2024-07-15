b39d0dbee8f4eeb399e8719d901691f10a1a10b1
JordanBandur <jordanbandur@hotmail.ca>
2024-07-15 10:12:44 -0600

    Add basic code for Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
 function Nav() {
   return (
     <nav>
-
+      <img src="images/Logo.svg"></img>
+      <ul>
+        <li>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Menu</a>
+          <a href="">Reservations</a>
+          <a href="">Order Online</a>
+          <a href="">Login</a>
+        </li>
+      </ul>
     </nav>
   );
 }
