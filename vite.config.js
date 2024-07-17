54fc6ef37afa2c9e7dceb15356bbb1f1e63ae3bb
JordanBandur <jordanbandur@hotmail.ca>
2024-07-17 09:39:10 -0600

    Fix Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3006,
     open: false,
   },
+  publicDir: 'public',
   build: {
     outDir: 'build',
   },
