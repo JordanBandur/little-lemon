b6e33e689a2aee250a098e1f53d1cadabc7dfe78
JordanBandur <jordanbandur@hotmail.ca>
2024-07-17 23:58:11 -0600

    Add code to About

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../assets/styles/About.scss';
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About</h2>
+      <p>Little Lemon</p>
+      <p>Chicago</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+      <figure>
+        <img src="adrian-mario.jpg" alt="Adrian and Mario" />
+      </figure>
+    </section>
+  );
+};
+
+export default About;
