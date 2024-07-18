d295a14200bbb1dc63bf9f331672a21f3e498270
JordanBandur <jordanbandur@hotmail.ca>
2024-07-17 23:58:43 -0600

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
 import Footer from './components/Footer';
 import './assets/styles/global.scss';
 
@@ -6,6 +10,12 @@ function App() {
   return (
     <>
       <Header />
+      <section className='container'>
+        <Hero />
+        <Highlights />
+        <Testimonials />
+        <About />
+      </section>
       <Footer />
     </>
   );
