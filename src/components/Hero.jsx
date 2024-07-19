004e8191dd02639ffe4e6ddbcd3c81646216054a
JordanBandur <jordanbandur@hotmail.ca>
2024-07-18 23:52:18 -0600

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
         <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        <button className="button primary">Reserve a Table</button>
+        <button className="button secondary">Reserve a Table</button>
       </section>
       <figure className="hero-image">
         <img src="images/restaurant-food.jpg" alt="Mediterranean food" />
