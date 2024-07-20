a52507815502044d846e22140c83d7ab0df8915b
JordanBandur <jordanbandur@hotmail.ca>
2024-07-20 11:24:15 -0600

    Added hamburger menu for small screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="header-nav">
-      <img src="images/Logo.svg" alt="Little Lemon logo"></img>
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#menu">Menu</a>
-          <a href="#reservations">Reservations</a>
-          <a href="#order">Order Online</a>
-          <a href="#login">Login</a>
-        </li>
+      <img src="images/Logo.svg" alt="Little Lemon logo" className="logo" />
+      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle menu">
+        &#9776;
+      </button>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#reservations">Reservations</a></li>
+        <li><a href="#order">Order Online</a></li>
+        <li><a href="#login">Login</a></li>
       </ul>
     </nav>
   );
