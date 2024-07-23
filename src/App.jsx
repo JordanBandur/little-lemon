187817db6da675b223b37b5dffebe0ccf703520b
JordanBandur <jordanbandur@hotmail.ca>
2024-07-22 23:47:58 -0600

    Add route for Reservation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Testimonials from './components/Testimonials';
 import About from './components/About';
 import Footer from './components/Footer';
+import Reservation from './components/Reservation';
 import './assets/styles/global.scss';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <section className='container'>
-        <Hero />
-        <Highlights />
-        <Testimonials />
-        <About />
-      </section>
+      <Routes>
+        <Route path="/reserve" element={<Reservation />} />
+        <Route path="/" element={
+          <section className='container'>
+            <Hero />
+            <Highlights />
+            <Testimonials />
+            <About />
+          </section>
+        } />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
