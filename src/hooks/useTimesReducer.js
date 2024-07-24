b0234bec4df4f7453e0838bd67c7f6864ed5620a
JordanBandur <jordanbandur@hotmail.ca>
2024-07-24 10:40:04 -0600

    Import api

--- src/hooks/useTimesReducer.js
+++ src/hooks/useTimesReducer.js
@@ -1,12 +1,16 @@
-export const initializeTimes = () => [
-  '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM',
-  '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM', '09:00 PM'
-];
+import { fetchAPI } from '../api';
+
+export const initializeTimes = () => {
+  const today = new Date();
+  const times = fetchAPI(today);
+  return times;
+};
 
 export const updateTimes = (state, action) => {
   switch (action.type) {
     case 'UPDATE_TIMES':
-      return initializeTimes();
+      const times = fetchAPI(new Date(action.payload));
+      return times;
     default:
       return state;
   }
