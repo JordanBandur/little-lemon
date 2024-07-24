1fc44c1768e0a3f77f53b1fbe4904e6864093039
JordanBandur <jordanbandur@hotmail.ca>
2024-07-24 10:09:17 -0600

    Add test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Reserve a Table link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Reserve a Table/i);
   expect(linkElement).toBeInTheDocument();
 });
